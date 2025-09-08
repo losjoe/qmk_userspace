6923356d2bde7e5e5402e5636003f6fa60d6735b
Rahkzar <rahkzar@gmail.com>
2025-09-08 14:55:24 -0400

    minor edits

--- keyboards/keychron/k3_max/ansi/rgb/keymaps/losjoe/keymap.c
+++ keyboards/keychron/k3_max/ansi/rgb/keymaps/losjoe/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,      KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
      LT(EXTEND, KC_BSPC),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
      MT(MOD_LSFT, KC_LPRN),               KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            MT(MOD_RSFT, KC_RPRN),  KC_UP,    KC_END,
-     KC_LCTL,     KC_LALT,  KC_LGUI,                                LT(FN, KC_SPC),                                 MO(MOD1), MO(MOD2), KC_CAPS,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+     KC_LCTL,     KC_LALT,  KC_LGUI,                                LT(SYM, KC_SPC),                                 MO(MOD1), MO(MOD2), KC_CAPS,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [EXTEND] = LAYOUT_ansi_84(
      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,      KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
      LT(EXTEND, KC_BSPC),  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_ENT,             KC_HOME,
      MT(MOD_LSFT, KC_LPRN),               KC_X,     KC_C,     KC_D,     KC_V,     KC_Z,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,            MT(MOD_RSFT, KC_RPRN),  KC_UP,    KC_END,
-     KC_LCTL,     KC_LALT,  KC_LGUI,                                LT(FN, KC_SPC),                                 MO(MOD1), MO(MOD2), KC_CAPS,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+     KC_LCTL,     KC_LALT,  KC_LGUI,                                LT(SYM, KC_SPC),                                 MO(MOD1), MO(MOD2), KC_CAPS,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [SYM] = LAYOUT_ansi_84(
      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
